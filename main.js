d9985ed57fb02daee8b3faab9b70eaf1ff085acb
simratminhas <145588260+simratminhas@users.noreply.github.com>
2023-11-07 09:21:54 -0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,16 @@
+//Create A Quiz By Simrat
+
+//Event Listener
+document.getElementById("btn").addEventListener("click", checkAnswers);
+
+function checkAnswers() {
+  //INPUT
+  let answer1 = +document.getElementById("incredibles").value;
+  let answer2 = +document.getElementById("mickey").value;
+  let answer3 = +document.getElementById("dory").value;
+  let answer4 = +document.getElementById("lionKing").value;
+  let outputEl = document.getElementById("output");
+
+  if (answer1 === edna) {
+  }
+}
